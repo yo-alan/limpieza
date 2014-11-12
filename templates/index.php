08d43cb89e707604ea6a9e5700e95e118adf3b05
Alan <marchanalan@hotmail.com>
2014-11-12 13:44:35 -0300

    Estoy probando nuevos estilos para la aplicacion.

--- templates/index.php
+++ templates/index.php
@@ -9,54 +9,48 @@
 		<script language="JavaScript" src="js/twitter-bootstrap-hover-dropdown.min.js"></script>
 	</head>
 	<body>
-		<header>
-			<div class="container">
-				<nav class="navbar navbar-default" role="navigation">
-					<div class="container-fluid">
-						<div class="navbar-header">
-							<a class="navbar-brand" href="index.php">Inicio</a>
-						</div>
-						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-							<ul class="nav navbar-nav">
-								<li class="dropdown">
-									<a href="elemento.php" class="dropdown-toggle" data-hover="dropdown">Elementos <span class="caret"></span></a>
-									<ul class="dropdown-menu" role="menu">
-										<li><a href="elemento.php?action=ingreso">Registrar ingreso</a></li>
+		<div class="jumbotron">
+			<nav class="navbar navbar-default" role="navigation">
+				<div class="container-fluid">
+					<div class="navbar-header">
+						<a class="navbar-brand" href="index.php">Inicio</a>
+					</div>
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<ul class="nav navbar-nav">
+							<li class="dropdown">
+								<a href="elemento.php" class="dropdown-toggle" data-hover="dropdown">Elementos <span class="caret"></span></a>
+								<ul class="dropdown-menu" role="menu">
+									<li><a href="elemento.php?action=ingreso">Registrar ingreso</a></li>
 <!--
-										<li><a href="elemento.php?action=retiro">Registrar retiro</a></li>
+									<li><a href="elemento.php?action=retiro">Registrar retiro</a></li>
 -->
-									</ul>
+								</ul>
 <!--
-								</li>
-								<li class="dropdown">
-									<a href="agente.php" class="dropdown-toggle" data-hover="dropdown">Agentes <span class="caret"></span></a>
-									<ul class="dropdown-menu" role="menu">
-										<li><a href="agente.php?action=agregar">Agregar</a></li>
-									</ul>
-								</li>
+							</li>
+							<li class="dropdown">
+								<a href="agente.php" class="dropdown-toggle" data-hover="dropdown">Agentes <span class="caret"></span></a>
+								<ul class="dropdown-menu" role="menu">
+									<li><a href="agente.php?action=agregar">Agregar</a></li>
+								</ul>
+							</li>
 -->
-							</ul>
-						</div><!-- /.navbar-collapse -->
-					</div><!-- /.container-fluid -->
-				</nav>
+						</ul>
+					</div><!-- /.navbar-collapse -->
+				</div><!-- /.container-fluid -->
+			</nav>
+		</div>
+		<?php if(isset($estado)): ?>
+			<div class="container col-md-6 col-md-offset-3 alert alert-<?php echo $estado; ?>" style="width: 50%">
+				<p class="text-<?php echo $estado; ?> text-center">
+					<strong><?php echo $mensaje; ?></strong>
+				</p>
 			</div>
-		</header>
-		<article>
-			<?php if(isset($estado)): ?>
-				<div class="container col-md-6 col-md-offset-3 alert alert-<?php echo $estado; ?>" style="width: 50%">
-					<p class="text-<?php echo $estado; ?> text-center">
-						<strong><?php echo $mensaje; ?></strong>
-					</p>
-				</div>
-			<?php endif; ?>
-			<div class="col-md-12">
-				<?php  ?>
-			</div>
-		</article>
-		<footer>
-			<div class="container">
-				
-			</div>
-		</footer>
+		<?php endif; ?>
+		<div class="col-md-12">
+			<?php  ?>
+		</div>
+		<div class="container">
+			
+		</div>
 	</body>
 </html>
